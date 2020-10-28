ecece1a74e230cf588d47219863d86809de103d3
Bert Scholten <bscholten@dat.nl>
2020-10-28 16:58:43 +0100

    Adding AERIUS 2020 (nature) dataset
    Named the schema/code nature2020 in case we ever want to add the airquality version (which is slightly different for road profiles and doesn't really need other sector stuff).

---
+++ source/database/src/data/sql/nature2020/dataset_metadata.sql
@@ -0,0 +1,2 @@
+INSERT INTO nature2020.dataset_metadata (key, value) VALUES ('MINIMUM_YEAR', '2018');
+INSERT INTO nature2020.dataset_metadata (key, value) VALUES ('MAXIMUM_YEAR', '2030');
---
+++ source/database/src/main/sql/load/02-main_datasets.sql
@@ -0,0 +1,2 @@
+CREATE SCHEMA nature2020;
+{import_common_into_schema 'template/', 'nature2020'}
