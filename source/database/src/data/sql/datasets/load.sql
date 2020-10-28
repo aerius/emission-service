75e3dfc28efd1154fa0d21cdfc0ccad256fc948e
Bert Scholten <bscholten@dat.nl>
2020-10-28 16:58:43 +0100

    Adding AERIUS 2020 (nature) dataset
    Named the schema/code nature2020 in case we ever want to add the airquality version (which is slightly different for road profiles and doesn't really need other sector stuff).

--- source/database/src/data/sql/datasets/load.sql
+++ source/database/src/data/sql/datasets/load.sql
@@ -1,5 +1,7 @@
+-- Dataset voor development
+
 INSERT INTO datasets (code, name, schema_name, current, description)
-	VALUES ('dev', 'current development', 'dev', true, 'Dataset to be used for current development');
+	VALUES ('dev', 'current development', 'dev', false, 'Dataset to be used for current development');
 
 INSERT INTO i18n_datasets (code, language_code, name, description)
 	VALUES ('dev', 'nl', 'Huidige ontwikkeling', 'Dataset die gebruikt wordt bij huidige ontwikkelingen');
@@ -9,3 +11,18 @@ INSERT INTO i18n_datasets (code, language_code, name, description)
 {import_common 'dev/dataset_metadata.sql'}
 {import_common 'dev/sectors'}
 {import_common 'dev/emission_factors'}
+
+
+-- Dataset(s) voor AERIUS 2020
+
+INSERT INTO datasets (code, name, schema_name, current, description)
+	VALUES ('nature2020', 'AERIUS 2020 nature', 'nature2020', true, 'Dataset used in AERIUS 2020, nature profile, released on 15 october 2020');
+
+INSERT INTO i18n_datasets (code, language_code, name, description)
+	VALUES ('nature2020', 'nl', 'AERIUS 2020 natuur', 'Dataset zoals gebruikt in AERIUS 2020, natuur profiel, publiek beschikbaar vanaf 15 oktober 2020');
+INSERT INTO i18n_datasets (code, language_code, name, description)
+	VALUES ('nature2020', 'en', 'AERIUS 2020 nature', 'Dataset used in AERIUS 2020, nature profile, released on 15 october 2020');
+
+{import_common 'nature2020/dataset_metadata.sql'}
+{import_common 'nature2020/sectors'}
+{import_common 'nature2020/emission_factors'}
