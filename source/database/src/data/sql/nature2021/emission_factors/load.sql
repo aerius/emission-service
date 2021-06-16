ff9a8c1f8b782bb8d8aaba187a1b52c3f0888a42
eltee1 <38038189+eltee1@users.noreply.github.com>
2021-06-16 12:31:45 +0200

    actualised import files for RAV. AER3-460.

--- source/database/src/data/sql/nature2021/emission_factors/load.sql
+++ source/database/src/data/sql/nature2021/emission_factors/load.sql
@@ -4,27 +4,27 @@ BEGIN; SELECT setup.ae_load_table('nature2021.road_speed_profiles', '{data_folde
 BEGIN; SELECT setup.ae_load_table('nature2021.road_category_emission_factors', '{data_folder}/public/road_category_emission_factors_20200929.txt'); COMMIT;
 
 /* Farm animal category and -lodging data */
-BEGIN; SELECT setup.ae_load_table('nature2021.farm_animal_categories', '{data_folder}/public/farm_animal_categories_20200914.txt'); COMMIT;
-BEGIN; SELECT setup.ae_load_table('nature2021.farm_lodging_system_definitions', '{data_folder}/public/farm_lodging_system_definitions_20200914.txt'); COMMIT;
+BEGIN; SELECT setup.ae_load_table('nature2021.farm_animal_categories', '{data_folder}/public/farm_animal_categories_20210607.txt'); COMMIT;
+BEGIN; SELECT setup.ae_load_table('nature2021.farm_lodging_system_definitions', '{data_folder}/public/farm_lodging_system_definitions_20210607.txt'); COMMIT;
 
-BEGIN; SELECT setup.ae_load_table('nature2021.farm_lodging_types', '{data_folder}/public/farm_lodging_types_20200914.txt'); COMMIT;
-BEGIN; SELECT setup.ae_load_table('nature2021.farm_lodging_type_emission_factors', '{data_folder}/public/farm_lodging_type_emission_factors_20200914.txt'); COMMIT;
-BEGIN; SELECT setup.ae_load_table('nature2021.farm_lodging_types_other_lodging_type', '{data_folder}/public/farm_lodging_types_other_lodging_type_20200914.txt'); COMMIT;
-BEGIN; SELECT setup.ae_load_table('nature2021.farm_lodging_types_to_lodging_system_definitions', '{data_folder}/public/farm_lodging_types_to_lodging_system_definitions_20200914.txt'); COMMIT;
+BEGIN; SELECT setup.ae_load_table('nature2021.farm_lodging_types', '{data_folder}/public/farm_lodging_types_20210607.txt'); COMMIT;
+BEGIN; SELECT setup.ae_load_table('nature2021.farm_lodging_type_emission_factors', '{data_folder}/public/farm_lodging_type_emission_factors_20210607.txt'); COMMIT;
+BEGIN; SELECT setup.ae_load_table('nature2021.farm_lodging_types_other_lodging_type', '{data_folder}/public/farm_lodging_types_other_lodging_type_20210607.txt'); COMMIT;
+BEGIN; SELECT setup.ae_load_table('nature2021.farm_lodging_types_to_lodging_system_definitions', '{data_folder}/public/farm_lodging_types_to_lodging_system_definitions_20210607.txt'); COMMIT;
 
-BEGIN; SELECT setup.ae_load_table('nature2021.farm_additional_lodging_systems', '{data_folder}/public/farm_additional_lodging_systems_20200914.txt'); COMMIT;
-BEGIN; SELECT setup.ae_load_table('nature2021.farm_additional_lodging_system_emission_factors', '{data_folder}/public/farm_additional_lodging_system_emission_factors_20200914.txt'); COMMIT;
-BEGIN; SELECT setup.ae_load_table('nature2021.farm_additional_lodging_systems_to_lodging_system_definitions', '{data_folder}/public/farm_additional_lodging_systems_to_lodging_system_definitions_20200914.txt'); COMMIT;
+BEGIN; SELECT setup.ae_load_table('nature2021.farm_additional_lodging_systems', '{data_folder}/public/farm_additional_lodging_systems_20210607.txt'); COMMIT;
+BEGIN; SELECT setup.ae_load_table('nature2021.farm_additional_lodging_system_emission_factors', '{data_folder}/public/farm_additional_lodging_system_emission_factors_20210607.txt'); COMMIT;
+BEGIN; SELECT setup.ae_load_table('nature2021.farm_additional_lodging_systems_to_lodging_system_definitions', '{data_folder}/public/farm_additional_lodging_systems_to_lodging_system_definitions_20210607.txt'); COMMIT;
 BEGIN; SELECT setup.ae_load_table('nature2021.farm_lodging_types_to_additional_lodging_systems', '{data_folder}/public/farm_lodging_types_to_additional_lodging_systems_20200914.txt'); COMMIT;
 
-BEGIN; SELECT setup.ae_load_table('nature2021.farm_reductive_lodging_systems', '{data_folder}/public/farm_reductive_lodging_systems_20200914.txt'); COMMIT;
-BEGIN; SELECT setup.ae_load_table('nature2021.farm_reductive_lodging_system_reduction_factors', '{data_folder}/public/farm_reductive_lodging_system_reduction_factors_20200914.txt'); COMMIT;
-BEGIN; SELECT setup.ae_load_table('nature2021.farm_reductive_lodging_systems_to_lodging_system_definitions', '{data_folder}/public/farm_reductive_lodging_systems_to_lodging_system_definitions_20200914.txt'); COMMIT;
-BEGIN; SELECT setup.ae_load_table('nature2021.farm_lodging_types_to_reductive_lodging_systems', '{data_folder}/public/farm_lodging_types_to_reductive_lodging_systems_20200914.txt'); COMMIT;
+BEGIN; SELECT setup.ae_load_table('nature2021.farm_reductive_lodging_systems', '{data_folder}/public/farm_reductive_lodging_systems_20210607.txt'); COMMIT;
+BEGIN; SELECT setup.ae_load_table('nature2021.farm_reductive_lodging_system_reduction_factors', '{data_folder}/public/farm_reductive_lodging_system_reduction_factors_20210607.txt'); COMMIT;
+BEGIN; SELECT setup.ae_load_table('nature2021.farm_reductive_lodging_systems_to_lodging_system_definitions', '{data_folder}/public/farm_reductive_lodging_systems_to_lodging_system_definitions_20210607.txt'); COMMIT;
+BEGIN; SELECT setup.ae_load_table('nature2021.farm_lodging_types_to_reductive_lodging_systems', '{data_folder}/public/farm_lodging_types_to_reductive_lodging_systems_20210607.txt'); COMMIT;
 
-BEGIN; SELECT setup.ae_load_table('nature2021.farm_lodging_fodder_measures', '{data_folder}/public/farm_lodging_fodder_measures_20150623_2.txt'); COMMIT;
-BEGIN; SELECT setup.ae_load_table('nature2021.farm_lodging_fodder_measure_reduction_factors', '{data_folder}/public/farm_lodging_fodder_measure_reduction_factors_20150623.txt'); COMMIT;
-BEGIN; SELECT setup.ae_load_table('nature2021.farm_lodging_fodder_measures_animal_category', '{data_folder}/public/farm_lodging_fodder_measures_animal_category_20150625_2.txt'); COMMIT;
+BEGIN; SELECT setup.ae_load_table('nature2021.farm_lodging_fodder_measures', '{data_folder}/public/farm_lodging_fodder_measures_20210607.txt'); COMMIT;
+BEGIN; SELECT setup.ae_load_table('nature2021.farm_lodging_fodder_measure_reduction_factors', '{data_folder}/public/farm_lodging_fodder_measure_reduction_factors_20210607.txt'); COMMIT;
+BEGIN; SELECT setup.ae_load_table('nature2021.farm_lodging_fodder_measures_animal_category', '{data_folder}/public/farm_lodging_fodder_measures_animal_category_20210607.txt'); COMMIT;
 
 /* Farmland categories */
 BEGIN; SELECT setup.ae_load_table('nature2021.farmland_categories', '{data_folder}/temp/temp_farmland_categories_20200527.txt'); COMMIT;
