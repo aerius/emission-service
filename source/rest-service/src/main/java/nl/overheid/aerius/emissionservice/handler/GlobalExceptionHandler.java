8b2aca16e0f4cde0e9fadf9b198ac34662917759
Bert Scholten <bscholten@dat.nl>
2023-12-18 12:34:22 +0100

    Ensuring error responses are in json format
    GlobalExceptionHandler itself not directly required, but gives us a place for our own exceptions if those need mapping.

---
+++ source/rest-service/src/main/java/nl/overheid/aerius/emissionservice/handler/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright the State of the Netherlands
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+ */
+package nl.overheid.aerius.emissionservice.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+/**
+ * Handler to ensure exceptions are returned in json form, following RFC 7807.
+ *
+ * If needed, our own exceptions can be customized in this class.
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+}
